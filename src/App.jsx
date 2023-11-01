c5acbba86aa8852d6da0ba8a623aef9304bff3d3
charan-777 <godenacharanteja@gmail.com>
2023-11-01 06:55:55 -0700

    reviews

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Reviews from './Review';
+
+function App() {
+  return (
+    <div className="mybox">
+     <h2> Edupoly Reviews</h2>
+     <Reviews></Reviews>
+    </div>
+  );
+}
+
+export default App;
