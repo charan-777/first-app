cd089a1e3be27515fcb39a13a01fb573630b5723
charan-777 <godenacharanteja@gmail.com>
2023-11-01 22:34:32 -0700

    accord

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Question from './question';
+
+function App() {
+  return (
+    <div className='main '>
+    <div className='mydiv'>
+      <h3>Question And Answers About Login</h3>
+      <Question></Question>
+    </div>
+    </div>
+  );
+}
+
+export default App;
