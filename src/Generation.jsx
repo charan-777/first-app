407f215313d280682766d201832a65ca3012c2d6
charan-777 <godenacharanteja@gmail.com>
2023-11-07 23:37:58 -0800

    multi-list

---
+++ src/Generation.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+function List(){
+    return(
+       <div className="fgroup">
+       
+        <div className="list-group">
+          <a href="#" class="list" aria-current="true">
+            Group-1
+          </a>
+          <a href="#" class="list-group-item list-group-item-action">India</a>
+          <a href="#" class="list-group-item list-group-item-action">Srilanka</a>
+          <a href="#" class="list-group-item list-group-item-action ">southafrica</a>
+          <a class="list-group-item list-group-item-action disabled">bangladesh</a>
+        
+      </div>
+      
+        <div class="list-group">
+          <a href="#" class="list" aria-current="true">
+            Group-2
+          </a>
+          <a href="#" class="list-group-item list-group-item-action">russia</a>
+          <a href="#" class="list-group-item list-group-item-action">ukrain</a>
+          <a href="#" class="list-group-item list-group-item-action">itali</a>
+          <a class="list-group-item list-group-item-action disabled">mayanmar</a>
+      
+      </div>
+    
+        <div class="list-group">
+          <a href="#" class="list" aria-current="true">
+            Group-3
+          </a>
+          <a href="#" class="list-group-item list-group-item-action">newzeland</a>
+          <a href="#" class="list-group-item list-group-item-action">westindies</a>
+          <a href="#" class="list-group-item list-group-item-action">englad</a>
+          <a class="list-group-item list-group-item-action disabled">scotland</a>
+        
+      </div>
+      
+        <div class="list-group">
+          <a href="#" class="list" aria-current="true">
+            Group-4
+          </a>
+          <a href="#" class="list-group-item list-group-item-action">australia</a>
+          <a href="#" class="list-group-item list-group-item-action">canada</a>
+          <a href="#" class="list-group-item list-group-item-action">portugal</a>
+          <a class="list-group-item list-group-item-action disabled">chile</a>
+       
+      </div>
+      
+        <div class="list-group">
+          <a href="#" class="list" aria-current="true">
+            Group-5
+          </a>
+          <a href="#" class="list-group-item list-group-item-action">Canada</a>
+          <a href="#" class="list-group-item list-group-item-action">USA</a>
+          <a href="#" class="list-group-item list-group-item-action">China</a>
+          <a class="list-group-item list-group-item-action disabled">Russia</a>
+        </div>
+      
+      
+        <div class="list-group">
+          <a href="#" class="list" aria-current="true">
+            Group-6
+          </a>
+          <a href="#" class="list-group-item list-group-item-action">Canada</a>
+          <a href="#" class="list-group-item list-group-item-action">USA</a>
+          <a href="#" class="list-group-item list-group-item-action">China</a>
+          <a class="list-group-item list-group-item-action disabled">Russia</a>
+       
+      </div>
+     
+        <div class="list-group">
+          <a href="#" class="list" aria-current="true">
+            Group-7
+          </a>
+          <a href="#" class="list-group-item list-group-item-action">Canada</a>
+          <a href="#" class="list-group-item list-group-item-action">USA</a>
+          <a href="#" class="list-group-item list-group-item-action">China</a>
+          <a class="list-group-item list-group-item-action disabled">Russia</a>
+      </div>
+     
+        <div class="list-group">
+          <a href="#" class="list" aria-current="true">
+            Group-8
+          </a>
+          <a href="#" class="list-group-item list-group-item-action">India</a>
+          <a href="#" class="list-group-item list-group-item-action">Srilanka</a>
+          <a href="#" class="list-group-item list-group-item-action">southafrica</a>
+          <a class="list-group-item list-group-item-action disabled">bangladesh</a>
+      </div>
+      
+        <div class="list-group">
+          <a href="#" class="list" aria-current="true">
+            Group-9
+          </a>
+          <a href="#" class="list-group-item list-group-item-action">Canada</a>
+          <a href="#" class="list-group-item list-group-item-action">USA</a>
+          <a href="#" class="list-group-item list-group-item-action">China</a>
+          <a class="list-group-item list-group-item-action disabled">Russia</a>
+        </div>
+    
+      
+        <div class="list-group">
+          <a href="#" class="list" aria-current="true">
+            Group-10
+          </a>
+          <a href="#" class="list-group-item list-group-item-action">Canada</a>
+          <a href="#" class="list-group-item list-group-item-action">USA</a>
+          <a href="#" class="list-group-item list-group-item-action">China</a>
+          <a class="list-group-item list-group-item-action disabled">Russia</a>
+        </div>
+    </div>
+     
+    )
+}
+export default List
