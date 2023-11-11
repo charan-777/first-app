86d66508d9aff399ef5eab5ed8a241717845f7b3
charan-777 <godenacharanteja@gmail.com>
2023-11-10 21:44:05 -0800

    accord

---
+++ src/Accodion.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Showpara() {
+   
+    return (
+       <div>
+        </div>
+    )
+}
+export default Showpara
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Showpara from './Accodion';
+
+function App() {
+  return (
+    <div className="App">
+     <Showpara></Showpara>
+    </div>
+  );
+}
+
+export default App;
