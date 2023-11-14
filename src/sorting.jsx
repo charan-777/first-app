e1f4b66803cf5d7254184bfa6c462b2bbf6f7ca3
charan-777 <godenacharanteja@gmail.com>
2023-11-13 22:30:52 -0800

    table-sorting

---
+++ src/sorting.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+function Array(){
+    var [players,setPlayers]=React.useState( [
+        {
+          firstname:'Ravi',
+          lastname:'Sastri',
+          gender:'male'
+        },
+        {
+          firstname:'Sachin',
+          lastname:'Tendulkar',
+          gender:'male'
+        },
+        {
+          firstname:'Mithali',
+          lastname:'Raj',
+          gender:'female'
+        },
+        {
+          firstname:'Rohit',
+          lastname:'Sharma',
+          gender:'male'
+        },
+        {
+          firstname:'Smriti',
+          lastname:'Mandhana',
+          gender:'female'
+        },
+        {
+          firstname:'Virat',
+          lastname:'Kohli',
+          gender:'male'
+        },
+      ]);
+      function sortfn(keys,order){
+        var temp=([...players])
+        temp.sort((a,b)=>{
+            if(order==="asc"){
+            if(a[keys]>b[keys]){
+                return 1
+            }
+            else{
+                return-1
+            }
+        }
+        if(order==="dsc"){
+            if (a[keys]>b[keys]) {
+                return -1
+            }
+            else{
+                return 1
+            }
+        }
+        })
+        setPlayers([...temp])
+      }
+    
+    return(
+        <div className="main">
+            <div className="sec">
+            <h1>Students</h1>
+            <button onClick={()=>{sortfn("firstname","asc")}}>sorting by Firstname(A-Z)</button>
+            <button onClick={()=>{sortfn("firstname","dsc")}}>sorting by Firstname(Z-A)</button>
+            <button onClick={()=>{sortfn("lastname","asc")}}>sorting by Lastname(A-Z)</button>
+            <button onClick={()=>{sortfn("lastname","dsc")}}>sorting by Lastname(Z-A)</button>
+            <button onClick={()=>{sortfn("gender","asc")}}>sorting by Gender(A-Z)</button>
+            <button onClick={()=>{sortfn("gender","dsc")}}>sorting by Gender(Z-A)</button>
+            </div>
+            <br />
+            {
+                <table border="1px solid" width="100%">
+                    <thead>
+                        <th>Firstname</th>
+                        <th>Lastname</th>
+                        <th>Gender</th>
+                    </thead>
+                   <tbody>
+                    {
+                        players.map((a)=>{
+                            return <tr>
+                                <td>{a.firstname}</td>
+                                <td>{a.lastname}</td>
+                                <td>{a.gender}</td>
+                            </tr>
+                        })
+                    }
+                   </tbody>
+                </table>
+            }
+        </div>
+       
+    )
+}
+export default Array
